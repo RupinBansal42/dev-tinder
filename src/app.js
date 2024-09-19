6d91b0d436219def5e3ef7b3d684f80baece4788
RupinBansal42 <bansal.rupin3@gmail.com>
2024-09-19 08:09:58 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -7,9 +7,15 @@ app.get("/user/:userID/:name", (req, res) => {
   res.send("User Details Recieved");
 });
 
-app.post("/user", (req, res) => {
-  res.send("Saved User Details successfully");
-});
+app.post("/user", [
+  (req, res, next) => {
+    // res.send("Saved User Details successfully");
+    next();
+  },
+  (req, res) => {
+    res.send("New Data saved successfully");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
