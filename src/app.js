e2b80b943cafbfa31d8c903efda81391b0e72041
RupinBansal42 <bansal.rupin3@gmail.com>
2024-10-15 14:11:37 +0530

    beautify

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-app.use(cors( {
-  origin : "http://localhost:5173",
-  credentials : true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
